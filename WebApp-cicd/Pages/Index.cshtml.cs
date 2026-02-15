005704ee5a4077c9ba87dc4664bbc4719618f424
ravik3592 <56120798+ravik3592@users.noreply.github.com>
2026-02-15 14:14:12 +0400

    Add project files.

---
+++ WebApp-cicd/Pages/Index.cshtml.cs
@@ -0,0 +1,13 @@
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+
+namespace WebApp_cicd.Pages
+{
+    public class IndexModel : PageModel
+    {
+        public void OnGet()
+        {
+
+        }
+    }
+}
